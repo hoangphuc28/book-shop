dc83a32d3dff381c3195941b48bd07a6771530fa
hoangphuc28 <zhoangp@gmail.com>
2024-05-21 21:49:53 +0700

    refactor: change apps name
    from 'admin' to 'admin-mvc' and 'customer' to 'user-api'

---
+++ apps/user-api/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { CoreModule } from '@book-shop/libs';
+import { BooksModule } from './books/books.module';
+import { AboutModule } from './about/about.module';
+import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { CartModule } from './cart/cart.module';
+@Module({
+  imports: [
+    CoreModule,
+    BooksModule,
+    AboutModule,
+    UserModule,
+    OrderModule,
+    CartModule,
+  ],
+})
+export class AppModule {}
