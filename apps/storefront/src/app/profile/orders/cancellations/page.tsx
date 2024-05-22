e6b28cab45219bb3ec9e5fa12358a7f985c6ee32
hoangphuc28 <zhoangp@gmail.com>
2024-05-22 09:30:49 +0700

    refactor(orders/*): change import path name

--- apps/storefront/src/app/profile/orders/cancellations/page.tsx
+++ apps/storefront/src/app/profile/orders/cancellations/page.tsx
@@ -1,5 +1,6 @@
-import ListOrders from "@/app/components/listOrders";
+
 import { Fragment } from "react";
+import ListOrders from "../../../components/listOrders";
 
 export default function Cancellations() {
     const src = "https://cdn.shopify.com/s/files/1/0533/2089/files/design-books-the-design-of-everyday-things-book-cover.jpg?v=1587988106";
