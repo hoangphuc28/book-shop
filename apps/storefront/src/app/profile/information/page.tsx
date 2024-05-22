517d1c1e609ee7f701dfc304f192fa4e0cbeb7e0
hoangphuc28 <zhoangp@gmail.com>
2024-05-22 09:24:26 +0700

    style(information/page.tsx): add information page

---
+++ apps/storefront/src/app/profile/information/page.tsx
@@ -0,0 +1,41 @@
+export default function Profile() {
+    return (
+        <div className="col-span-9 shadow rounded px-6 pt-5 pb-7">
+            <h4 className="text-lg font-medium capitalize mb-4">
+                Profile information
+            </h4>
+            <div className="space-y-4">
+                <div className="grid grid-cols-2 gap-4">
+                    <div>
+                        <label htmlFor="first">First name</label>
+                        <input type="text" name="first" id="first" className="input-box" />
+                    </div>
+                    <div>
+                        <label htmlFor="last">Last name</label>
+                        <input type="text" name="last" id="last" className="input-box" />
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-4">
+                    <div>
+                        <label htmlFor="email">Email Address</label>
+                        <input type="email" name="email" id="email" className="input-box" />
+                    </div>
+                    <div>
+                        <label htmlFor="phone">Phone number</label>
+                        <input type="text" name="phone" id="phone" className="input-box" />
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="address">Address</label>
+                    <input type="text" name="address" id="address" className="input-box" />
+                </div>
+            </div>
+            <div className="mt-4">
+                <button type="submit" className="py-3 px-4 text-center text-white bg-primary border border-primary rounded-md hover:bg-transparent hover:text-primary transition font-medium">save
+                    changes</button>
+            </div>
+        </div>
+
+    )
+}
